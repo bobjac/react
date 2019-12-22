76db6ab807f2a0c3fa0ba8415ab0ae9f350c3543
Bob Jacobs <bob.jacobs@outlook.com>
2019-12-22 16:06:11 -0500

    adding the handleClick function to the Game class

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -30,21 +30,7 @@ import './index.css';
   }
   
   class Board extends React.Component {
-    handleClick(i) {
-        const history = this.state.history;
-        const current = history[history.length - 1];
-        const squares = current.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-          return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-          history: history.concat([{
-            squares: squares,
-          }]),
-          xIsNext: !this.state.xIsNext,
-        });
-      }
+    
   
     renderSquare(i) {
       return (
@@ -88,6 +74,22 @@ import './index.css';
             xIsNext : true,
         }
     }
+
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+          return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+          history: history.concat([{
+            squares: squares,
+          }]),
+          xIsNext: !this.state.xIsNext,
+        });
+      }
 
     render() {
         const history = this.state.history;
